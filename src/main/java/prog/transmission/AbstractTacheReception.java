7f54de39f6f9c125ccbaac3e958b38ad514106a9
ronanPoinsignon <ronan.poinsignon@laposte.net>
2025-12-09 16:39:21 +0100

    ajout import

--- src/main/java/prog/transmission/AbstractTacheReception.java
+++ src/main/java/prog/transmission/AbstractTacheReception.java
@@ -1,8 +1,11 @@
 package prog.transmission;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 import javafx.concurrent.Task;
 
