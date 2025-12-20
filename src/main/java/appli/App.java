2b954394d3666bb66bfe50b16817d02cea4b6305
ronanPoinsignon <ronan.poinsignon@laposte.net>
2025-12-20 02:37:10 +0100

    fix import inutile

--- src/main/java/appli/App.java
+++ src/main/java/appli/App.java
@@ -7,7 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import prog.controllers.ControllerVideo;
+import javafx.stage.StageStyle;
 
 /**
  *
