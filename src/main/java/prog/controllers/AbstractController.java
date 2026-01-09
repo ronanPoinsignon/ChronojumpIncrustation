472ffad11f7fea06358ffb68e4423dd7e532582e
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-01-09 13:40:01 +0100

    séparation des éléments avec un controller abstrait pour plus de flexibilité + rename controller

---
+++ src/main/java/prog/controllers/AbstractController.java
@@ -0,0 +1,198 @@
+package prog.controllers;
+
+import javafx.animation.FadeTransition;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import prog.observableproperties.StringObsProperty;
+import prog.transmission.tache.AbstractTacheReception;
+import prog.transmission.tache.RawTacheReception;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+
+public abstract class AbstractController implements Initializable {
+
+    private static final int FADE_DURATION = 200;
+
+    private static final String INIT_IP_ADRESSE = "192.168.1.20";
+//	private static final String INIT_IP_ADRESSE = "169.254.122.66";
+
+    // 20% de la hauteur de l'écran
+    protected static final int BASE_WIDTH = 1280;
+
+    private boolean isFullScreen = false;
+    private double coordX, coordY;
+    private AtomicReference<Scene> atomicScene = new AtomicReference<>();
+
+    @FXML
+    private AnchorPane idAnchorBase;
+
+    public AbstractController() {
+        AbstractTacheReception.setAdresse(INIT_IP_ADRESSE);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        idAnchorBase.sceneProperty().addListener((obsScene, oldVScene, newScene) -> onSceneUpdate(newScene));
+    }
+
+    protected void bindLabelSize(Label label, int defaultSize, Scene scene) {
+        ChangeListener<? super Number> listener = (obs, oldV, newV) -> {
+            if(scene == null) {
+                return;
+            }
+
+            double width = scene.getWidth();
+            if(width == 0) {
+                return;
+            }
+
+            setLabelTextSize(label, defaultSize, scene.getWidth());
+        };
+
+        scene.widthProperty().addListener(listener);
+    }
+
+    protected void setLabelTextSize(final Label label, final int defaultSize, double sceneWidth) {
+        final double widthRatio = sceneWidth/ BASE_WIDTH;
+        double textSize = widthRatio * defaultSize;
+        label.setFont(Font.font(label.getFont().getFamily(), FontWeight.findByName(label.getFont().getStyle()), textSize));
+    }
+
+    protected void setImageSize(ImageView imageView, Scene scene, int defaultSize) {
+        imageView.fitWidthProperty().bind(scene.widthProperty().multiply(defaultSize).divide(AbstractController.BASE_WIDTH));
+        imageView.fitHeightProperty().bind(scene.widthProperty().multiply(defaultSize).divide(AbstractController.BASE_WIDTH));
+    }
+
+    protected ChangeListener<? super Boolean> addFadeTransition(final Node node) {
+        return (obs, oldV, newV) -> {
+            final FadeTransition transition = new FadeTransition(Duration.millis(FADE_DURATION), node);
+            if(newV) {
+                transition.setFromValue(0);
+                transition.setToValue(1);
+            } else {
+                transition.setFromValue(1);
+                transition.setToValue(0);
+            }
+            transition.play();
+        };
+    }
+
+    protected String capitalizeFirstOnly(String value) {
+        if(value == null) {
+            return null;
+        }
+        if(value.trim().isEmpty()) {
+            return "";
+        }
+
+        return value.substring(0, 1).toUpperCase() + value.substring(1).toLowerCase();
+    }
+
+    protected void bind(final Label label, final RawTacheReception tacheReception) {
+        this.bind(label, tacheReception.valueProperty());
+    }
+
+    protected void bind(final Label label, final ReadOnlyObjectProperty<String> property) {
+        label.textProperty().bind(property);
+        setVisibilityEvent(label);
+    }
+
+    protected void bind(Label label, StringObsProperty stringObsProperty, Function<String, String> supplier) {
+        label.textProperty().bind(Bindings.createStringBinding(() -> {
+            StringProperty property = stringObsProperty.getValue();
+            if(property == null) {
+                return "";
+            }
+
+            String value = property.getValue();
+            if(value == null) {
+                return "";
+            }
+
+            String trimValue = property.getValue().trim();
+            if(trimValue.isEmpty()) {
+                return "";
+            }
+
+            return supplier.apply(trimValue);
+        }, stringObsProperty.getValue()));
+        this.setVisibilityEvent(label);
+    }
+
+    protected void bind(Label label, StringObsProperty stringProperty) {
+        bind(label, stringProperty, String::trim);
+    }
+
+    protected void setVisibilityEvent(Label label)  {
+        label.visibleProperty().bind(Bindings.when(
+                        label.textProperty().isEmpty().or(label.textProperty().isEqualTo("0").or(label.textProperty().isEqualTo("0.0").or(label.textProperty().isEqualTo("0.00")))))
+                .then(false)
+                .otherwise(true));
+        label.visibleProperty().addListener((obs, oldV, newV) -> {
+            final FadeTransition transition = new FadeTransition(Duration.millis(FADE_DURATION), label);
+            if(newV) {
+                transition.setFromValue(0);
+                transition.setToValue(1.0);
+            } else {
+                transition.setFromValue(1);
+                transition.setToValue(0);
+            }
+            transition.play();
+        });
+    }
+
+    protected void addEvent(final Stage stage) {
+        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+        stage.getScene().setOnKeyPressed(evt -> {
+            if(evt.getCode() == KeyCode.F12 || isFullScreen && evt.getCode() == KeyCode.ESCAPE) {
+                switchFullScreen(stage);
+            }
+        });
+        idAnchorBase.setOnMousePressed(evt -> {
+            coordX = evt.getX();
+            coordY = evt.getY();
+            if(evt.getClickCount()%2 == 0) {
+                switchFullScreen(stage);
+            }
+        });
+        idAnchorBase.setOnMouseDragged(evt -> {
+            if(isFullScreen) {
+                evt.consume();
+                return;
+            }
+            stage.setX(evt.getScreenX() - coordX);
+            stage.setY(evt.getScreenY() - coordY);
+        });
+    }
+
+    protected void switchFullScreen(final Stage stage) {
+        stage.setFullScreen(isFullScreen = !isFullScreen);
+    }
+
+    protected AtomicReference<Scene> getAtomicScene() {
+        return atomicScene;
+    }
+
+    protected void onSceneUpdate(Scene scene) {
+        atomicScene.set(scene);
+        scene.windowProperty().addListener((obsWindow, oldWindow, newWindow) -> addEvent((Stage) newWindow));
+    }
+}
---
+++ src/main/java/prog/controllers/SHFControllerIncrustation.java
@@ -0,0 +1,136 @@
+package prog.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import prog.transmission.EventObserver;
+
+public class SHFControllerIncrustation extends AbstractController {
+
+	private final EventObserver eventObserver = EventObserver.getInstance();
+
+	@FXML
+	private Label idNumeroEpreuve;
+	@FXML
+	private Label idLieu;
+	@FXML
+	private Label idChrono;
+	@FXML
+	private Label idPenalite;
+	@FXML
+	private Label idDossard;
+	@FXML
+	private Label idNomCheval;
+	@FXML
+	private Label idRaceCheval;
+	@FXML
+	private Label idPereCheval;
+	@FXML
+	private Label idMereCheval;
+	@FXML
+	private Label idPereMereCheval;
+	@FXML
+	private Label idPrenomCavalier;
+	@FXML
+	private Label idNomCavalier;
+	@FXML
+	private ImageView idImageCavalier;
+	@FXML
+	private Pane idPaneCavalier;
+	@FXML
+	private ImageView idLogo;
+	@FXML
+	private Pane idPaneLogo;
+	@FXML
+	private ImageView idImageChrono;
+	@FXML
+	private Pane idPaneChrono;
+	@FXML
+	private GridPane idGridpaneChrono;
+	@FXML
+	private GridPane idGridpaneInfo;
+
+	@Override
+	public void initialize(final URL location, final ResourceBundle resources) {
+		super.initialize(location, resources);
+
+		// Gestion des events de récéption des informations
+
+		this.bind(idChrono, eventObserver.getChrono());
+		this.bind(idDossard, eventObserver.getDossard());
+		this.bind(idPenalite, eventObserver.getPenalite());
+
+		bind(idLieu, eventObserver.getLieuEpreuve());
+		bind(idNumeroEpreuve, eventObserver.getNumeroEpreuve());
+
+		this.bind(idNomCheval, eventObserver.getChevalName());
+		this.bind(idRaceCheval, eventObserver.getChevalRace());
+		this.bind(idPereCheval, eventObserver.getChevalPere(), value -> "Père : " + value);
+		this.bind(idMereCheval, eventObserver.getChevalMere(), value -> "Mère : " + value);
+		this.bind(idPereMereCheval, eventObserver.getChevalPereMere(), value -> "Père de mère : " + value.replace("(Mme\\.)|(M\\.)", "").trim());
+		this.bind(idNomCavalier, eventObserver.getCavalier(), (value) -> {
+			String[] parts = value.split(" ", -1);
+			String nom = parts.length == 2 ? parts[1].split("_")[0] : "";
+			return nom.toUpperCase();
+		});
+		this.bind(idPrenomCavalier, eventObserver.getCavalier(), (value) -> {
+			String[] parts = value.split("_");
+			String prenom = parts.length > 1 ? parts[parts.length - 1] : value;
+			return capitalizeFirstOnly(prenom);
+		});
+
+		// gestion visibilité pane principaux
+
+		idGridpaneChrono.visibleProperty().bind(idChrono.visibleProperty());
+		idChrono.visibleProperty().addListener(addFadeTransition(idGridpaneChrono));
+
+		BooleanBinding allLabelsVisible = Bindings.or(Bindings.createBooleanBinding(() -> false), idNomCheval.visibleProperty())
+				.or(idRaceCheval.visibleProperty())
+				.or(idPereCheval.visibleProperty())
+				.or(idMereCheval.visibleProperty())
+				.or(idPereMereCheval.visibleProperty())
+				.or(idNomCavalier.visibleProperty())
+				.or(idDossard.visibleProperty());
+
+		idGridpaneInfo.visibleProperty().bind(allLabelsVisible);
+		allLabelsVisible.addListener(addFadeTransition(idGridpaneInfo));
+
+		//		Gestion responsive
+		idLogo.fitWidthProperty().bind(idPaneLogo.widthProperty().multiply(0.8));
+		idLogo.layoutXProperty().bind(idPaneLogo.widthProperty().multiply(0.5).subtract(idLogo.fitWidthProperty().divide(2)));
+
+		idImageChrono.layoutXProperty().bind(idPaneChrono.widthProperty().multiply(0.5).subtract(idImageChrono.fitWidthProperty().divide(2)));
+		idImageChrono.layoutYProperty().bind(idPaneChrono.heightProperty().multiply(0.5).subtract(idImageChrono.fitHeightProperty().divide(2)));
+		idImageCavalier.layoutXProperty().bind(idPaneCavalier.widthProperty().multiply(0.5).subtract(idImageCavalier.fitWidthProperty().divide(2)));
+		idImageCavalier.layoutYProperty().bind(idPaneCavalier.heightProperty().multiply(0.5).subtract(idImageCavalier.fitHeightProperty().divide(2)));
+	}
+
+	@Override
+	protected void onSceneUpdate(Scene scene) {
+		super.onSceneUpdate(scene);
+
+		setImageSize(idImageChrono, scene, 35);
+		setImageSize(idImageCavalier, scene, 64);
+
+		this.bindLabelSize(idLieu, 16, scene);
+		this.bindLabelSize(idNumeroEpreuve, 16, scene);
+		this.bindLabelSize(idPereCheval, 16, scene);
+		this.bindLabelSize(idMereCheval, 16, scene);
+		this.bindLabelSize(idPereMereCheval, 16, scene);
+		this.bindLabelSize(idPrenomCavalier, 16, scene);
+		this.bindLabelSize(idNomCavalier, 16, scene);
+		this.bindLabelSize(idPenalite, 23, scene);
+		this.bindLabelSize(idNomCheval, 23, scene);
+		this.bindLabelSize(idRaceCheval, 23, scene);
+		this.bindLabelSize(idDossard, 23, scene);
+		this.bindLabelSize(idChrono, 23, scene);
+	}
+}
