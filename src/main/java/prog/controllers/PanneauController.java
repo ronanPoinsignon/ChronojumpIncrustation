61ac6249da9f059740322caa92f4575819315d17
ronanPoinsignon <ronan.poinsignon@laposte.net>
2026-01-12 17:01:37 +0100

    ajout parallélogrammes sur label

--- src/main/java/prog/controllers/PanneauController.java
+++ src/main/java/prog/controllers/PanneauController.java
@@ -1,12 +1,14 @@
 package prog.controllers;
 
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+import javafx.scene.shape.SVGPath;
 import prog.transmission.EventObserver;
 
-import java.awt.*;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,6 +28,23 @@ public class PanneauController extends AbstractController {
     private Label idPereCheval;
     @FXML
     private Label idMereCheval;
+    @FXML
+    private SVGPath idSVGCavalier;
+    @FXML
+    private SVGPath idSVGCheval;
+    @FXML
+    private SVGPath idSVGPenalite;
+    @FXML
+    private SVGPath idSVGChrono;
+    @FXML
+    private GridPane idGridpaneCavalier;
+    @FXML
+    private GridPane idGridpaneCheval;
+    @FXML
+    private GridPane idGridpanePenalite;
+    @FXML
+    private GridPane idGridpaneChrono;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -37,6 +56,33 @@ public class PanneauController extends AbstractController {
         this.bind(idDossard, eventObserver.getDossard());
         this.bind(idMereCheval, eventObserver.getChevalMere());
         this.bind(idChrono, eventObserver.getChrono());
+
+        // gestion parallelogramme
+
+        bindParallelogramme(idGridpaneCavalier, idSVGCavalier);
+        bindParallelogramme(idGridpaneCheval, idSVGCheval);
+        bindParallelogramme(idGridpanePenalite, idSVGPenalite);
+        bindParallelogramme(idGridpaneChrono, idSVGChrono);
+    }
+
+    private void bindParallelogramme(Region region, SVGPath svgPath) {
+        double angle = 10;
+
+        ChangeListener<Number> listener = (obs, oldV, newV) -> svgPath.setContent(computeSVGContent(region, angle));
+        region.widthProperty().addListener(listener);
+        region.heightProperty().addListener(listener);
+    }
+
+    private String computeSVGContent(Region region, double angle) {
+        double x1 = computeX(region.getWidth(), region.getHeight(), angle);
+        double length = region.getWidth() - x1;
+        return "M " + length + ",0 L " + region.getWidth() + ",0 L " + x1 + "," + region.getHeight() + " L 0," + region.getHeight() + " Z";
+    }
+
+    private double computeX(double width, double height, double angle) {
+        double radiansAngle = Math.toRadians(angle);
+        double lengthSide = height * Math.cos(radiansAngle);
+        return width - Math.sin(radiansAngle) * lengthSide;
     }
 
     @Override
